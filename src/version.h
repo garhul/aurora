505e922c49c0e0590cffceaf99ce7e72e200ce00
Adrian Gesto <adrian.gesto@gmail.com>
2022-04-28 12:18:48 +0200

    restored settings file, removed unused config file

--- src/version.h
+++ src/version.h
@@ -1,10 +1,10 @@
 
 #ifndef BUILD_NUMBER
-  #define BUILD_NUMBER "167"
+  #define BUILD_NUMBER "172"
 #endif
 #ifndef VERSION
-  #define VERSION "v0.1.167 - 2022-04-28 11:13:46.773971"
+  #define VERSION "v0.1.172 - 2022-04-28 11:32:40.673429"
 #endif
 #ifndef VERSION_SHORT
-  #define VERSION_SHORT "v0.1.167"
+  #define VERSION_SHORT "v0.1.172"
 #endif
